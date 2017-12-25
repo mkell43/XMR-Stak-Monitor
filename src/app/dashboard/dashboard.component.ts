558935b47971ce9626b08f6f9b118e1906704c1b
Raylehnhoff <smdaegan@gmail.com>
2017-12-24 23:43:20 -0700

    shares

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -35,7 +35,9 @@ export class DashboardComponent {
       this.rollup.push({
         "minerName": miner.minerName,
         hashRateTotals: miner.data.hashrate.total.slice(0),
-        errors: miner.data.results.error_log.length
+        errors: miner.data.results.error_log.length,
+        sharesGood: miner.data.results.shares_good,
+        sharesTotal: miner.data.results.shares_total
       });
 
       index = this.rollup.findIndex((item) => item.minerName == "Total");
@@ -46,19 +48,28 @@ export class DashboardComponent {
       var total = {
         "minerName": "Total",
         hashRateTotals: [],
-        errors: 0
+        errors: 0,
+        sharesGood:0,
+        sharesTotal:0
       };
 
-      var accumulator0 = 0;
-      var accumulator1 = 0;
-      var accumulator2 = 0;
-      var accumulator3 = 0;
+      var accumulator0 = 0,
+       accumulator1 = 0,
+       accumulator2 = 0,
+       accumulator3 = 0,
+       accumulator4 = 0,
+       accumulator5 = 0
+
       for (let miner of this.rollup) {
         accumulator0 += miner.hashRateTotals[0];
         accumulator1 += miner.hashRateTotals[1]
         accumulator2 += miner.hashRateTotals[2]
         accumulator3 += miner.errors;
+        accumulator4 += miner.sharesGood,
+        accumulator5 += miner.sharesTotal
       }
+      total.sharesGood = accumulator4;
+      total.sharesTotal = accumulator5;
       total.hashRateTotals = [accumulator0, accumulator1, accumulator2];
       total.errors = accumulator3;
 
