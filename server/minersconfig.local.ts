cdd46d98cf1b92e80be19cee18cd08c2d17d74a4
Mike Keller <mike.k@blu-web.com>
2017-12-27 11:36:49 -0600

    Init testing config.

--- server/minersconfig.local.ts
+++ server/minersconfig.local.ts
@@ -4,10 +4,8 @@ export const miners = {
         /*
         The app will request the API endpoint of each address listed here in the format below:
         */
-        {"name":"Miner", "url":"http://192.168.86.72:10080", "username":"", "password":""},
-        {"name":"Server", "url":"http://192.168.86.4:60080", "username":"admin", "password":"admin"},
-        {"name":"Desktop", "url":"http://localhost:10080", "username":"", "password":""}
+        {"name":"Digger", "url":"http://digger.mikekeller.name:8080", "username":"", "password":""},
 
-        
+
     ]
 }
